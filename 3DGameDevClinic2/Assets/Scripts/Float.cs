b9589c8e184bf98c3709173afe211bf77c4788ad
DESKTOP-5OQPAIO\Plush Von Plush <lucasremington.gamedesigner@gmail.com>
2018-02-14 19:43:07 -0500

    updated test scene with float script

---
+++ 3DGameDevClinic2/Assets/Scripts/Float.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+	public float spinSpeed = 10f;
+	float originalY;
+	public float floatStrength = 1; 
+	public bool yesRotate;
+
+	void Start()
+	{
+		this.originalY = this.transform.position.y;
+
+	}
+
+	void Update()
+	{
+		transform.position = new Vector3(transform.position.x,
+			originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
+			transform.position.z);
+		if (yesRotate == true) {
+			transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+		}
+	}
+}
